c6a8f0c3ba898e4f4f483e30c2c32fcd547261f9
brune <brune@LAPTOP-8B7I48TA>
2020-10-05 18:17:39 -0300

    Funcionando o banco

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 public class Principal {
 
 	public static void main(String[] args) throws IOException {
-		//Permissoes viewpermissoes = new Permissoes();
-		//viewpermissoes.ViewPermissoes();
+		// Permissoes viewpermissoes = new Permissoes();
+		// viewpermissoes.ViewPermissoes();
 		Login log = new Login();
 		log.viewLogin(null);
 	}
