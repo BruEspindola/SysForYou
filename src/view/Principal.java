f77b9158e9f400f9ae58c5e008f3a34e5e0135e1
dev <dev@dev-RV411-RV511-E3511-S3511-RV711-E3411>
2020-10-08 18:18:21 -0300

    vai chara

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -6,9 +6,6 @@ public class Principal {
 
 	public static void main(String[] args) throws IOException {
 		Login log = new Login();
-		//Usuarios us = new Usuarios();
-		//String permissao = "GETI";
-		//us.ViewUsuarios(permissao);
 		log.viewLogin();
 	}
 
