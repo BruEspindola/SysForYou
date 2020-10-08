56f9e2d61e1d8c55ff73e9dbd9f053bf982b172e
dev <dev@dev-RV411-RV511-E3511-S3511-RV711-E3411>
2020-10-08 18:23:28 -0300

    finalizado

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Principal {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException { //o sistema começa aqui :3
 		Login log = new Login();
 		log.viewLogin();
 	}
